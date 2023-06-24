937e41f3374a4b44e25f4e44711076a6ee390a4f
poppotaufikgalli <poppo.galli@gmail.com>
2023-06-24 21:54:19 +0700

    upddate firestore

--- server/api/fcmApi.ts
+++ server/api/fcmApi.ts
@@ -1,39 +1,69 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth } from "firebase/auth"
+import { getFirestore, collection, addDoc, serverTimestamp } from "firebase/firestore"; 
+//import { useNuxtApp } from "#imports";
+
 export default defineEventHandler(async (event) => {
-    const reqData = await readBody(event)
-    const { from, to, tipe, title, body } = reqData
-    //return { body }
+  const config = useRuntimeConfig()
 
-    const FIREBASE_API_KEY = process.env.FIREBASE_API_KEY
-    
-    const message = {
-      registration_ids: [to],
-      notification: {
-        title: tipe == 'chat' ? title : tipe,
-        body: body,
-        vibrate: 1,
-        sound: 1,
-        show_in_foreground: true,
-        priority: "high",
-        content_available: true
-      },
-      data: {
-        tipe: tipe,
-        ref_token: from,
-        text: body,
-      },
-    };
+  //console.log(config.public)
 
-    let headers = new Headers({
-      "Content-Type": "application/json",
-      Authorization: "key=" + FIREBASE_API_KEY
+  const firebaseConfig = config.public.firebaseConfig;
+  const app = initializeApp(firebaseConfig)
+  const auth = getAuth(app)
+  const firestore = getFirestore(app)
+
+  const reqData = await readBody(event)
+  const { from, from_id, to, tipe, title, body } = reqData
+
+  try {
+    const docRef = await addDoc(collection(firestore, tipe), {
+      messageText : body,
+      sentAt: serverTimestamp(),
+      sentBy: from_id,
+      tipe: tipe,
+      //sentTo: 'D'+costumer_id,
+      orderId: title,
     });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
 
-    let response = await fetch("https://fcm.googleapis.com/fcm/send", {
-      method: "POST",
-      headers,
-      body: JSON.stringify(message)
-    });
-    response = await response.json();
+  //return { body }
 
-    return from
+  const FIREBASE_API_KEY = process.env.FIREBASE_API_KEY
+  
+  const message = {
+    registration_ids: [to.trim()],
+    notification: {
+      title: tipe == 'chat' ? title : tipe,
+      body: body,
+      vibrate: 1,
+      sound: 1,
+      show_in_foreground: true,
+      priority: "high",
+      content_available: true
+    },
+    data: {
+      tipe: tipe,
+      ref_token: from,
+      from_id: from_id,
+      text: body,
+    },
+  };
+
+  let headers = new Headers({
+    "Content-Type": "application/json",
+    Authorization: "key=" + FIREBASE_API_KEY
+  });
+
+  let response = await fetch("https://fcm.googleapis.com/fcm/send", {
+    method: "POST",
+    headers,
+    body: JSON.stringify(message)
+  });
+  response = await response.json();
+
+  return response
 })
